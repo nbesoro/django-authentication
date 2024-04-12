719d82e846475119738ba50980372489f87395cf
nbesoro <ibrasoro04@gmail.com>
2024-04-12 21:28:05 +0000

    feat: base code

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-
-from django.contrib.auth.models import User
-from .forms import RegisterForm, LoginForm
+from django.contrib.auth import logout
 
 
 def index(request):
@@ -10,28 +7,11 @@ def index(request):
 
 
 def register_view(request):
-    form = RegisterForm()
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("login")
-    return render(request, "register.html", {"form": form})
+    return render(request, "register.html")
 
 
 def login_view(request):
-    form = LoginForm()
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data["username"]
-            password = form.cleaned_data["password"]
-            user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)
-                return redirect("home")
-
-    return render(request, "login.html", {"form": form})
+    return render(request, "login.html")
 
 
 # logout page
--- core/settings.py
+++ core/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "authentication",
 ]
-CRISPY_ALLOWES_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
